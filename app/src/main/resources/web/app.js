78c3992f429a0b2c19e65baddebe745705248458
kriskata <kriskata5000@gmail.com>
2023-09-27 17:24:43 +0300

    Buttons for video and music download are done in the website.

--- app/src/main/resources/web/app.js
+++ app/src/main/resources/web/app.js
@@ -1,21 +1,22 @@
 
 function addVideoButton(name, image_url) {
     var textnode = document.createElement("button")
-    textnode.innerHTML = name+" -> MP4 HIGH"
+    textnode.innerHTML = name+" | MP4 HIGH"
     textnode.setAttribute("id", "videobtn")  // #008000   #00FFFF  #FFFFFF
-    textnode.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;";
+    textnode.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: bold;-webkit-transition: .3s ease-in-out;transition: .3s ease-in-out;background-size: 100%;";
+
     document.getElementById("downloadbtn").appendChild(textnode)
 
     var textnode1 = document.createElement("button")
-    textnode1.innerHTML = name+" -> MP4 LOW"
+    textnode1.innerHTML = name+" | MP4 LOW"
     textnode1.setAttribute("id", "videobtn")
-    textnode1.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;"
+    textnode1.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: bold;-webkit-transition: .3s ease-in-out;transition: .3s ease-in-out;background-size: 100%;"
     document.getElementById("downloadbtn").appendChild(textnode1)
 
     var textnode2 = document.createElement("button")
-    textnode2.innerHTML = name+" -> MP3"
+    textnode2.innerHTML = name+" | MP3"
     textnode2.setAttribute("id", "videobtn")
-    textnode2.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;"
+    textnode2.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: bold;-webkit-transition: .3s ease-in-out;transition: .3s ease-in-out;background-size: 100%;"
     document.getElementById("downloadbtn").appendChild(textnode2)
 
     var newline = document.createElement("br");
