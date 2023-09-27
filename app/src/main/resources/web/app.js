65c9d3af772c9289956eb9d4c22eae608cfa437e
kriskata <kriskata5000@gmail.com>
2023-09-27 17:13:59 +0300

    Making buttons look cool.

--- app/src/main/resources/web/app.js
+++ app/src/main/resources/web/app.js
@@ -1,17 +1,36 @@
 
-function test() {
-    // Create an "li" node:
-    //const node = document.createElement("button");
+function addVideoButton(name, image_url) {
+    var textnode = document.createElement("button")
+    textnode.innerHTML = name+" -> MP4 HIGH"
+    textnode.setAttribute("id", "videobtn")  // #008000   #00FFFF  #FFFFFF
+    textnode.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;";
+    document.getElementById("downloadbtn").appendChild(textnode)
 
-    // Create a text node:
-    const textnode = document.createElement("button");
+    var textnode1 = document.createElement("button")
+    textnode1.innerHTML = name+" -> MP4 LOW"
+    textnode1.setAttribute("id", "videobtn")
+    textnode1.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;"
+    document.getElementById("downloadbtn").appendChild(textnode1)
 
-    // Append the text node to the "li" node:
-    //node.appendChild(textnode);
+    var textnode2 = document.createElement("button")
+    textnode2.innerHTML = name+" -> MP3"
+    textnode2.setAttribute("id", "videobtn")
+    textnode2.style.cssText = "text-align: center;border-color: blue;box-sizing: content-box;background-color: #FFFFFF;margin-left: auto;margin-right: auto;display: block;border-radius: 12px;font-size: 22px;cursor: pointer;background-image: url("+image_url+");background-repeat: no-repeat;background-position: left;padding-left: 250px;font-weight: 20vh;"
+    document.getElementById("downloadbtn").appendChild(textnode2)
+
+    var newline = document.createElement("br");
+    newline.setAttribute("id", "videoln")
+    document.getElementById("downloadbtn").appendChild(newline)
+}
 
 
-    textnode.innerHTML = "Name of video -> 0%";
+function removeVideoButtons() {
+   var all_buttons = document.getElementById("downloadbtn")
+   while (all_buttons.firstChild) {
+       all_buttons.removeChild(all_buttons.lastChild)
+   }
+}
 
-    // Append the "li" node to the list:
-    document.getElementById("downloadbtn").appendChild(textnode);
+function updateDownloadProgress(percent) {
+      document.getElementById("progress").innerHTML = percent
 }
